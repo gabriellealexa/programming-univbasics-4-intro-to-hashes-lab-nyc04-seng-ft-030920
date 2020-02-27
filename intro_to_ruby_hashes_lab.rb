46b1c9d180b986282bff46f668cdf7ea5a988bbc
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-27 08:25:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,15 +21,15 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  return hash[:key]
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
-  if hash[:key]
-    hash[:key] +=1
+  if hash[key]
+    hash[key] +=1
     return hash 
   else
-    hash[:key] = 1
+    hash[key] = 1
     return hash
 end
 end 
